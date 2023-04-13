2fa0cee150b3d434d48dabd808646fe1ad111a72
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-13 19:39:52 +0300

    add react-on-screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import TrackVisibility from "react-on-screen";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <TrackVisibility></TrackVisibility>
+      </Container>
+    </>
+  );
 }

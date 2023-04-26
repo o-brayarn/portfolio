a8e73c137e0e00b524f39ece7efd2e18f0e28921
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-26 09:13:19 +0300

    remove home from components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as About } from "./about/About";
 export { default as Socials } from "./socials/Socials";
 export { default as Footer } from "./footer/Footer";
-export { default as Home } from "./home/Home";
 export { default as NavBar } from "./navbar/NavBar";
 export { default as Projects } from "./projects/Projects";
 export { default as Services } from "./services/Services";

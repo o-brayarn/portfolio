2b012e4d2279ad5d86a0f3994fed8ddf4179207a
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-28 16:58:58 +0300

    include rotating text to exports

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as NavBar } from "./navbar/NavBar";
 export { default as Projects } from "./projects/Projects";
 export { default as Services } from "./services/Services";
 export { default as Skills } from "./skills/Skills";
+export { default as RotatingText } from "./rotating-text/RotatingText";

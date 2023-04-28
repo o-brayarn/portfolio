a9e81199db1406b159c84b4381ca9d3181d7853e
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-28 17:52:34 +0300

    add rotating-text component

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import './rotatingtext.css'
 
 const RotatingText = () => {
+    const ReactRotatingText = require('react-rotating-text');
   return (
-    <div>RotatingText</div>
+      <div className='rotating__text'>
+          <ReactRotatingText items={['Software Engineer','Full Stack Developer', 'Technical Writer', 'Open Source Contributor','Blockchain & Crypto Writer', 'Professional Coder']} />
+    </div>
   )
 }
 

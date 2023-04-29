752bcec1035c79d15224fce5a1578ef4c818334d
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-29 12:56:03 +0300

    refactor: import ReactRotatingText

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import ReactRotatingText from 'react-rotating-text'
 import './rotatingtext.css'
 
 const RotatingText = () => {
-    const ReactRotatingText = require('react-rotating-text');
   return (
       <div className='rotating__text'>
           <ReactRotatingText items={['Software Engineer','Full Stack Developer', 'Technical Writer', 'Open Source Contributor','Blockchain & Crypto Writer', 'Professional Coder']} />

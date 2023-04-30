362e95f8fd84562967e20e66d056a568beb1dcf6
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-30 15:19:39 +0300

    add placeholder to import file

--- src/pages/homepage/imports.js
+++ src/pages/homepage/imports.js
@@ -1,4 +1,5 @@
 import grad from "../../assets/grad.png";
 import Updated from "../../assets/Updated.pdf";
+import placeholder from "../../assets/placeholder.png";
 
-export { grad, Updated };
+export { grad, Updated, placeholder };

4434c70281f88b42d641314e2f3fa802ac662c7d
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-01 16:54:15 +0300

    add resume && contactme btns

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -11,15 +11,29 @@ export default function HomePage() {
             <h1>Hi, I'm <span>Brayarn</span> </h1>
             <RotatingText />
         </div>
+
         <div className='portfolio__home-content__details'>
-        <p>
-        A dedicated Software Engineer. Building Web applications with React, TypeScript, Ruby on Rails, JavaScript, MySQL/Postgres, among other frameworks and libraries.
-        </p>
+          <p>
+          A dedicated Software Engineer. Building Web applications with React, TypeScript, Ruby on Rails, JavaScript, MySQL/Postgres, among other frameworks and libraries.
+          </p>
         </div>
+
+        <div className='portfolio__buttons'>
+          <div className='resume__btn'>
+            <button type='button'>
+              <a href="https://docs.google.com/document/d/1SO2GqK0zZdbawB-l4kiCpDmmAQGCUABn1_J-aAHbfsU/edit?usp=sharing" target="_blank"  rel="noopener noreferrer">View Resume</a>
+            </button>
+          </div>
+          
+          <div className='contact__me-btn'>
+            <input type="button" value="Contact Me" />
+          </div>
+        </div>
+        
       </div>
 
       <div className='portfolio__home-image'>
-        <img src={placeholder} alt="grad" />
+        <img src={placeholder} alt="my profile" />
       </div>
      
       

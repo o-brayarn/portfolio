2541947a027fd6d01b6067242a84ec7dc88bfee3
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-04 06:43:14 +0300

    create services rendered containers

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,8 +3,11 @@ import './services.css';
 
 export default function Services() {
   return (
-    <div>
-      Services
+    <div className='portfolio__services'>
+      <div className='portfolio__services-frontend'></div>
+      <div className='portfolio__services-API'></div>
+      <div className='portfolio__services'></div>
+      <div className=''></div>
     </div>
   )
 }

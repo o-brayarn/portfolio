a341840da7d21965aeceebdc6fb20928629ca070
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-05 00:54:11 +0300

    begin working on services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './services.css';
+// import Card from 'react-bootstrap/Card';
+// import { CardGroup } from 'react-bootstrap';
 
 export default function Services() {
   return (
+    <>
+      <div className='main__services'>
+        <h1>What I do</h1>
     <div className='portfolio__services'>
       <div className='portfolio__services-frontend'></div>
       <div className='portfolio__services-API'></div>
       <div className='portfolio__services'></div>
       <div className=''></div>
+      </div>
     </div>
+    </>
   )
 }

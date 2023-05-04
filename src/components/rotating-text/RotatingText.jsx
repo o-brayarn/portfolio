70dc33848a149d3c5e9f75fef6533c00a7841b6f
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-04 07:05:56 +0300

    Modify Rotating Text items

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -5,7 +5,7 @@ import './rotatingtext.css'
 const RotatingText = () => {
   return (
       <div className='rotating__text'>
-          <ReactRotatingText items={['Software Engineer.','Full Stack Developer.', 'Technical Writer.', 'Open Source Contributor.','Blockchain & Crypto Writer.', 'Professional Coder.', 'Bringin your ideas to life!']} />
+          <ReactRotatingText items={['Software Engineer.','Full Stack Developer.', 'Open Source Contributor.', 'Bringing your ideas to life!']} />
     </div>
   )
 }

d532e0033c7863371942cdc496e5683b5219961e
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-05 23:15:03 +0300

    add service details

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import './services.css';
-// import Card from 'react-bootstrap/Card';
-// import { CardGroup } from 'react-bootstrap';
 
 export default function Services() {
   return (
     <>
       <div className='main__services'>
         <h1>What I do</h1>
-    <div className='portfolio__services'>
-      <div className='portfolio__services-frontend'></div>
-      <div className='portfolio__services-API'></div>
-      <div className='portfolio__services'></div>
-      <div className=''></div>
+        <div className='portfolio__services'>
+              <div className='portfolio__services-frontend'>
+                <h5>Web Application Development</h5>
+                <hr />
+                <p>Need a website for your business? I'm your guy. I develop market-defining web applications, both backend and frontend, that meet client specifications and consumer needs.</p>
+              </div>
+
+              <div className='portfolio__services-API'>
+                <h5>Restful API Design & Integration</h5>
+                <hr />
+                <p>I can develop highly secure, fast, reliable, and scalable Restful API, as well as integrate it with your web application.</p>
+              </div>
+          
+              <div className='portfolio__services-project'>
+                <h5>Project Management</h5>
+                <hr />
+                <p>I can manage a team of developers to accomplish tasks, meet deadlines, and achieve project goals.</p>
+              </div>
+        </div>
+        <hr className='services__end'/>
       </div>
-    </div>
+      
     </>
   )
 }

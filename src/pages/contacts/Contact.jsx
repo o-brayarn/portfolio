4cf90af365f51c587b4ec16fdebe5ce257658f56
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-09 20:02:42 +0300

    give contacts an id

--- src/pages/contacts/Contact.jsx
+++ src/pages/contacts/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css';
 
 export default function Contact() {
   return (
-    <div>
+    <div id='contact'>
       Open for opportunities. Let's connect and build something awesome together!
     </div>
   )

09713e71881d357f01b7931cbd0799c7409bdf71
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-10 14:25:00 +0300

    add socials as hyperlinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Socials from "../socials/Socials";
 import './footer.css';
+import logo from "../../assets/logo.png";
+
 
 export default function Footer() {
   let year = new Date().getFullYear();
   return (
     <>
+      <section className="portfolio__footer">
          <div className="main__footer">
             <div className="portfolio__brand">
               <img
@@ -18,32 +20,39 @@ export default function Footer() {
               <h5>Bringing your ideas to life</h5>
             </div>
           
-        <div className="footer__links">
-          <h5>Important Links</h5>
-          <ul>
-            <li> <a href="#home">Home</a></li>
-            <li> <a href="#about">About Me</a></li>
-            <li> <a href="#services">Services</a></li>
-            <li> <a href="#projects">Projects</a></li>
-            <li> <a href="#contact">Contact</a></li>
-          </ul>
-        
-        </div>
+            <div className="footer__links">
+              <h5>Links</h5>
+              <ul>
+                <li> <a href="#home">Home</a></li>
+                <li> <a href="#about">About Me</a></li>
+                <li> <a href="#services">Services</a></li>
+                <li> <a href="#projects">Projects</a></li>
+                <li> <a href="#contact">Contact</a></li>
+              </ul>
+            </div>
 
-        <div className="footer__socials">
-          <h5>Get in touch:</h5>
-         <Socials /> 
-        </div>
-        <div className="footer__content">
-            <p>
-            Copyright &copy; {year}. All rights reserved by{" "}
-            <span>
-              <a href="https://brayarn.vercel.app/">brayarn &trade;</a>
-            </span>
-          </p>
-        </div>
+            <div className="footer__socials">
+              <h5>Find me on:</h5>
+              <ul className="socials__links">
+                <li><a href="https://github.com/o-brayarn" target="_blank"  rel="noopener noreferrer">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/brayarn" target="_blank"  rel="noopener noreferrer">LinkedIn</a></li>
+                <li><a href="mailto: brayarnogeda@gmail.com" target="_blank"  rel="noopener noreferrer">Email</a></li>
+                <li><a href="https://twitter.com/o_brayarn" target="_blank"  rel="noopener noreferrer">Twitter</a></li>
+              </ul>
+            </div>
+          
+          </div>
+      
+          <div className="footer__copyright">
+              <p>
+              Copyright &copy; {year}. All rights reserved by{" "}
+              <span>
+                <a href="https://brayarn.vercel.app/">brayarn &trade;</a>
+              </span>
+            </p>
+          </div>
        
-      </div>
+      </section>
     </>
   );
 }

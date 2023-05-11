e1e7e8ec33c8f4e2fd5a4ca629587cc6aa349fad
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-11 15:24:16 +0300

    add contactme boiler plate

--- src/pages/contacts/Contact.jsx
+++ src/pages/contacts/Contact.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './contact.css';
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div id='contact'>
+      <h1 className="contact__me__heading">Contact Me</h1>
+
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+      </form>
+      
       Open for opportunities. Let's connect and build something awesome together!
+
+      <hr />
     </div>
   )
 }

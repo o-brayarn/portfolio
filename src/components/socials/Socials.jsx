ef804eee53d9648e5813a33153c05589f84342e7
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-13 21:44:55 +0300

    modify: socials color

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -8,25 +8,25 @@ export default function Socials() {
       <div className='socials__container'>
         <div className='socials__github'>
           <a href="https://github.com/o-brayarn" target="_blank"  rel="noopener noreferrer">
-          <AiFillGithub color="#083344"/>
+          <AiFillGithub color="#81afdd"/>
           </a>
         </div>
 
         <div className='socials__linkedin'>
           <a href="https://www.linkedin.com/in/brayarn" target="_blank"  rel="noopener noreferrer">
-            <AiFillLinkedin color="#083344"/>
+            <AiFillLinkedin color="#81afdd"/>
           </a>
         </div>
 
         <div className='socials__gmail'>
           <a href="mailto: brayarnogeda@gmail.com" target="_blank"  rel="noopener noreferrer">
-          <AiOutlineMail color="#083344"/>
+          <AiOutlineMail color="#81afdd"/>
           </a>
         </div>
 
         <div className='socials__twitter'>
             <a href="https://twitter.com/o_brayarn" target="_blank"  rel="noopener noreferrer">
-              <AiOutlineTwitter color="#083344"/>
+              <AiOutlineTwitter color="#81afdd"/>
             </a>
         </div>
       </div>

8020a00a0d5803609b9b4cf5a03142fa7b22639b
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-14 09:44:42 +0300

    add project heading

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,8 +3,12 @@ import './projects.css';
 
 export default function Projects() {
   return (
-    <div id="projects">
-      Projects
+    <div className='portfolio__projects' id="projects">
+      <div className="portfolio__projects-heading">
+        <h1 className="gradient__text">My Works</h1>
+      </div>
+      <div className="portfolio__projects-content"></div>
+      <hr />
     </div>
   )
 }

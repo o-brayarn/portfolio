732065da9ce33d37d35c75a2c7b11ec4849a9060
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-15 13:54:02 +0300

    create projects' containers

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,7 +7,12 @@ export default function Projects() {
       <div className="portfolio__projects-heading">
         <h1 className="gradient__text">My Works</h1>
       </div>
-      <div className="portfolio__projects-content"></div>
+      <div className="portfolio__projects-content">
+        <div className="portfolio__project-list">
+          <div className="portfolio__project-list__image"></div>
+          <div className="portfolio_project-list__details"></div>
+        </div>
+      </div>
       <hr />
     </div>
   )

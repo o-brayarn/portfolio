f9f9865ef2f70fd1c77feb0bd9398ccdde64de77
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-16 18:04:29 +0300

    add projects page to exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Contact } from "./contacts/Contact";
 export { default as HomePage } from "./homepage/HomePage";
 export { default as Footer } from "./footer/Footer";
+export { default as Projects } from "./projects/Projects";

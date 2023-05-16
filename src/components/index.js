f940e3d076b0990d8f7a1862a39bedb05b55cbfa
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-16 18:04:03 +0300

    add project list to exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as About } from "./about/About";
 export { default as Socials } from "./socials/Socials";
 export { default as NavBar } from "./navbar/NavBar";
-export { default as Projects } from "./projects/Projects";
+export { default as ProjectList } from "./projects/ProjectList";
 export { default as Services } from "./services/Services";
 export { default as Skills } from "./skills/Skills";
 export { default as RotatingText } from "./rotating-text/RotatingText";

73651a885ed3a79139e79345009e5e4a18ab0bfc
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-16 19:02:34 +0300

    import assets for project list

--- src/components/projects/imports.js
+++ src/components/projects/imports.js
@@ -1,3 +1,17 @@
 import braeda from "../../assets/braeda.png";
+import bookwarmers from "../../assets/bookwarmers.png";
+import eventsplug from "../../assets/eventsplug.png";
+import movieit from "../../assets/movieit.png";
+import mydoctor from "../../assets/mydoctor.png";
+import vallies from "../../assets/vallies.png";
+import portfolio from "../../assets/portfolio.png";
 
-export { braeda };
+export {
+  braeda,
+  bookwarmers,
+  eventsplug,
+  movieit,
+  mydoctor,
+  vallies,
+  portfolio,
+};

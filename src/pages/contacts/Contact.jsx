750b82b3f2e69b83966cd4c6568b7880e53a5328
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-18 10:10:08 +0300

    add contactme illustration

--- src/pages/contacts/Contact.jsx
+++ src/pages/contacts/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './contact.css';
 import Socials from '../../components/socials/Socials'
+import {contacts} from "./imports"
 
 
 export default function Contact() {
@@ -23,6 +24,7 @@ export default function Contact() {
       <h1 className="contact__me-heading gradient__text">Contact Me</h1>
       <div className="contact__me-details">
         <div className="contact__me-info">
+          <img src={contacts} alt="contactme illustration" />
           <h5 className='gradient__text'>Get in touch</h5>
           <p> <span> Email: {" "}</span>brayarnogeda@gmail.com</p>
           <p><span> Phone:{" "}</span>+254741280129</p>
